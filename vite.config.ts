70a636b99dfb9013354a643d83b51b913a82437b
sebas <charrintorres@gmail.com>
2025-11-23 16:25:24 -0600

    uwu4

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '', // Important for relative paths on GitHub Pages if not using custom domain
+  base: './', // Important for relative paths on GitHub Pages if not using custom domain
 });
